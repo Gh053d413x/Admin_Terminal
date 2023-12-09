ecbb61b63503a1e98d5f7abc9dbd5e4cb34d474b
Ghosted <100062083+Gh053d413x@users.noreply.github.com>
2023-12-09 00:01:38 -0700

    Updated adminTerminal.py
    Added a new logo for the Actual Terminal it's self
    Fixed Some Bugs where info.py would open even though other commands were entered. For Example: create --> info.py Expectation: create --> create.py
    Moved everything from info.py to adminTerminal.py due to the reason above

--- adminTerminal.py
+++ adminTerminal.py
@@ -1,3 +1,8 @@
+# Admin Terminal Alpha v1.1
+# Made by Ghosted
+# Software is free, you can edit and customize it 
+# because it is Open Source!
+
 import os
 import random
 import time
@@ -5,23 +10,35 @@ import sys
 from os.path import exists
 from pathlib import Path
 from tkinter import messagebox as msg
+import tkinter
 import traceback
 import secrets
+from venv import logger
 
 try:
     debug = False
     errorShow = False
+    instantInstall = False
 
     if __name__ == "__main__":
         if len(sys.argv) > 1:
             if sys.argv[1] == "-debug":
+                msg.showinfo("Admin Terminal", "Debug Mode Enabled")
                 debug = True
             elif sys.argv[1] == "-showError":
                 errorShow = True
+            elif sys.argv[1] == "-instantInstall":
+                instantInstall = True
+            elif sys.argv[1] == "-":
+                msg.showerror("Admin Terminal", "No Argument Provided")
+                print("No Argument Provided")
+                exit()
             else:
-                print("Invalid Argument")
+                msg.showerror("Admin Terminal", f"Invalid Argument: {sys.argv[1]}")
+                print(f"Invalid Argument, {sys.argv}")
+                exit()
         else:
-            print("No Argument Provided")
+            pass
 
     if debug == True:
         print("Program Started!")
@@ -45,9 +62,16 @@ try:
 
     class dependenciesExist:
         "This is where all of the dependencies will exist"
-        resourceFolderExists = exists(f"{winPath}/text_decor")
-        aboutFileExists = exists(f"{winPath}/info.py")
         createFileExists = exists(f"{winPath}/create.py")
+
+    logo = """
+             _           _         _______                  _             _ 
+    /\      | |         (_)       |__   __|                (_)           | |
+   /  \   __| |________  _ ____      | | ___ ____ ________  _ ____   ____| |
+  / /\ \ / _  |  _   _ \| |  _ \     | |/ _ \  __|  _   _ \| |  _ \ / _  | |
+ / ____ \ (_| | | | | | | | | | |    | |  __/ |  | | | | | | | | | | (_| | |
+/_/    \_\____|_| |_| |_|_|_| |_|    |_|\___|_|  |_| |_| |_|_|_| |_|\____|_|
+Alpha"""
 
     #files
     info_file = f"{winPath}/info.py"
@@ -57,45 +81,51 @@ try:
     encodedCode = secrets.token_urlsafe(512)
 
     os.system("cls")
+
+    #functions
+    
     def loading_bar(total):
         for i in range(total+1):
             time.sleep(random.random())
             print('\r[' + '-'*i + ' '*(total-i) + ']', end='')
+    
     def terminal():
+        print(logo)
         if errorShow == True:
             userInput = input(f"{text_decor.color.FAIL}Invalid Input has been entered\n{text_decor.style.END}>>> {text_decor.color.OKCYAN}")
         else:
             userInput = input(f">>> {text_decor.color.OKCYAN}")
-        if userInput == "about":
-            os.system(f"py {info_file}")
+            print(f"{text_decor.style.END}")
+        if userInput == "info":
+            info()
         elif userInput == "create":
             os.system(f"py {create_file}")
         else:
+            os.system("cls")
             print(f"{text_decor.color.FAIL}Invalid Command: {userInput} has ether a misspell or improper syntax{text_decor.style.END}")
             terminal()
+
+    def info():
+        os.system('cls')
+        print(f'''Admin Terminal v1.1
+Made by Ghosted
+Software is free, you can edit and customize it because it is Open Source!
+The other python files in {winPath} are all of the dependencies for the Admin Terminal
+Check out the github at https://github.com/Gh053d413x/Admin_Terminal''')
+        os.system('pause')
+        os.system("cls")
+        terminal()
+
     def createDependencies():
-        t = open(temp, "w")
-        t.write(encodedCode)
         print(f"Downloading and Copying files listed in {temp}. . .")
         loading_bar(15)
-        os.system("cls")
-        about = open(info_file, "w")
-        about.write("""try:
-    import os
-    os.system('cls')
-    print('''Admin Terminal\nVersion: 1.0\nAdmin Terminal is a terminal for admins\nPlease restart the Admin Terminal to continue''')
-    os.system('pause')
-    os.system('py ./adminTerminal.py')
-except:
-    os.system("./adminTerminal")""")
-        about.close()
 
         print("Installing files. . .")
         loading_bar(15)
 
         crt = open(create_file, "w")
-        crt.write("""try:
-    import os
+        crt.write("""import os
+try:
     from pathlib import Path
     os.system('cls')
     winPath = Path.cwd()
@@ -120,40 +150,48 @@ except:
         os.mkdir(path)
     else:
         os.system("cls")
-        os.system("py ./adminTerminal -errorShow")
+        os.system("py ./adminTerminal.py -errorShow")
 
-    os.system("py ./adminTerminal")
+    os.system("py ./adminTerminal.py")
 except:
-    os.system("py ./adminTerminal")""")
+    os.system("py ./adminTerminal.py")""")
         crt.close()
         os.system("cls")
         print("Cleaning Up. . .")
         loading_bar(20)
         os.system("cls")
-        t.close()
-        os.remove(temp)
         if debug == True:
             print("Terminal Activated!")
         terminal()
-    if dependenciesExist.aboutFileExists and dependenciesExist.createFileExists:
+
+    if dependenciesExist.createFileExists:
         if debug == True:
             print("Terminal Activated!")
         terminal()
     else:
-        if debug == True:
-            print("DEPENDENCIES MISSING!")
-        installConfirm = msg.askyesno("Admin Terminal", "One or More dependencies are not installed\nWould you like to install the dependencies?", icon="error")
-        if installConfirm == True:
+        if instantInstall == True:
             if debug == True:
                 print("Installing Dependencies")
             createDependencies()
         else:
-            quit()
+            if debug == True:
+                print("DEPENDENCIES MISSING!")
+            installConfirm = msg.askyesno("Admin Terminal", "One or More dependencies are not installed\nWould you like to install the dependencies?", icon="error")
+            if installConfirm == True:
+                if debug == True:
+                    print("Installing Dependencies")
+                createDependencies()
+            else:
+                quit()
 except Exception as err:
+    print(f"{text_decor.style.END}")
+    os.system("cls")
     lineNum = traceback.format_exc()
     if debug == True:
-        print(f"{text_decor.color.FAIL}Admin Terminal has been terminated due to an exception\nError: {err}\n\nFull Error: {lineNum}{text_decor.style.END}")
+        msg.showerror("Admin Terminal", f"Admin Terminal has been terminated due to an exception\nError: {err}\n\nFull Error: {lineNum}")
     else:
-        print(f"{text_decor.color.FAIL}Admin Terminal has been terminated from an unknown crash{text_decor.style.END}")
+        msg.showerror("Admin Terminal", "Admin Terminal has been terminated from an unknown crash")
 except KeyboardInterrupt:
-    print(f"{text_decor.color.FAIL}Admin Terminal has been terminated from a keyboard interruption{text_decor.style.END}")
+    print(f"{text_decor.style.END}")
+    os.system("cls")
+    msg.showerror("Admin Terminal", "Admin Terminal has been terminated from a keyboard interruption")
