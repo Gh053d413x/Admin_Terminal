5dd0b88be664c0d396b2d3c610be58ae9eafc7be
Ghosted <100062083+Gh053d413x@users.noreply.github.com>
2024-01-05 23:43:03 -0700

    Updated Admin Terminal
    Alpha 1.2
    Patch Notes
        -   Added Linux support
        -   [Experimental] Added Help Page
        -   Separated Version from Title
        -   Bug Fixes
        -   MAC Support is in development! Try this on MAC and see if it works!

--- adminTerminal.py
+++ adminTerminal.py
@@ -1,4 +1,4 @@
-# Admin Terminal Alpha v1.1
+# Admin Terminal Alpha v1.2
 # Made by Ghosted
 # Software is free, you can edit and customize it 
 # because it is Open Source!
@@ -13,7 +13,6 @@ from tkinter import messagebox as msg
 import tkinter
 import traceback
 import secrets
-from venv import logger
 
 try:
     debug = False
@@ -71,16 +70,29 @@ try:
   / /\ \ / _  |  _   _ \| |  _ \     | |/ _ \  __|  _   _ \| |  _ \ / _  | |
  / ____ \ (_| | | | | | | | | | |    | |  __/ |  | | | | | | | | | | (_| | |
 /_/    \_\____|_| |_| |_|_|_| |_|    |_|\___|_|  |_| |_| |_|_|_| |_|\____|_|
-Alpha"""
+"""
+
+    print("\n")
+
+    version = "Version 1.2"
+
+    if os.name == "posix":
+        py_command = "python3"
+        cls_command = "clear"
+        pause_command = "/bin/bash -c 'read -s -n 1 -p \"Press any key to continue...\"'"
+    elif os.name == "nt":
+        py_command = "py"
+        cls_command = "cls"
+        pause_command = "pause"
 
     #files
     info_file = f"{winPath}/info.py"
     create_file = f"{winPath}/create.py"
 
-    temp = f"{winPath}/tmp_{encodedName}"
+    temp = f"/tmp/{encodedName}"
     encodedCode = secrets.token_urlsafe(512)
 
-    os.system("cls")
+    os.system(f"{cls_command}")
 
     #functions
     
@@ -91,75 +103,115 @@ Alpha"""
     
     def terminal():
         print(logo)
+        print("Type 'help', 'patch' or 'info' for more information")
+        print(f"Alpha {version}")
         if errorShow == True:
             userInput = input(f"{text_decor.color.FAIL}Invalid Input has been entered\n{text_decor.style.END}>>> {text_decor.color.OKCYAN}")
+            print(f"{text_decor.style.END}")
         else:
             userInput = input(f">>> {text_decor.color.OKCYAN}")
             print(f"{text_decor.style.END}")
         if userInput == "info":
             info()
         elif userInput == "create":
-            os.system(f"py {create_file}")
+            os.system(f"{py_command} {create_file}")
+        elif userInput == "patch":
+            patch_notes()
+        elif userInput == "help":
+            help()
         else:
-            os.system("cls")
+            os.system(f"{cls_command}")
             print(f"{text_decor.color.FAIL}Invalid Command: {userInput} has ether a misspell or improper syntax{text_decor.style.END}")
             terminal()
 
+    def help():
+        os.system(f"{cls_command}")
+        print('''Welcome to the Admin Terminal Help
+This page is experimental so this page is subject to change, new commands will get added here when added
+Available Commands:
+    info - Shows info about the Admin Terminal
+    create - Creates files or folders in the specified directory
+    patch - Shows the patch notes
+    help - Shows this page''')
+        os.system(f"{pause_command}")
+        os.system(f"{cls_command}")
+        terminal()
+
+    def patch_notes():
+        os.system(f"{cls_command}")
+        print(f'''Alpha {version}
+Patch Notes
+    -   Added Linux support
+    -   [Experimental] Added Help Page
+    -   Separated Version from Title
+    -   Bug Fixes
+    -   MAC Support is in development! Try this on MAC and see if it works!''')
+        os.system(f"{pause_command}")
+        os.system(f"{cls_command}")
+        terminal()
+
     def info():
-        os.system('cls')
-        print(f'''Admin Terminal v1.1
+        os.system(f"{cls_command}")
+        print(f'''Alpha {version}
 Made by Ghosted
 Software is free, you can edit and customize it because it is Open Source!
 The other python files in {winPath} are all of the dependencies for the Admin Terminal
 Check out the github at https://github.com/Gh053d413x/Admin_Terminal''')
-        os.system('pause')
-        os.system("cls")
+        os.system(f"{pause_command}")
+        os.system(f"{cls_command}")
         terminal()
 
     def createDependencies():
         print(f"Downloading and Copying files listed in {temp}. . .")
         loading_bar(15)
 
+        os.system(cls_command)
         print("Installing files. . .")
         loading_bar(15)
 
         crt = open(create_file, "w")
         crt.write("""import os
+if os.name == "posix":
+    py_command = "python3"
+    cls_command = "clear"
+else:
+    py_command = "py"
+    cls_command = "cls"
+
 try:
     from pathlib import Path
-    os.system('cls')
+    os.system('clear')
     winPath = Path.cwd()
     userInput = input('''Enter the path you want the File/Folder to be in
-    >>> ''')
+>>> ''')
     if userInput == "":
-        os.system("py ./create.py")
+        os.system("{py_command} ./create.py")
     else:
         path = userInput
-    os.system('cls')
-
-    print("Path set to: " + path)
-    fileFolder = input('''Which do you want to create?
+    os.system(f'{cls_command}')
+    fileFolder = input(f'''Path is set to: {path}
+Which do you want to create?
     Available Options:
     * File
     * Folder
-    >>> ''')
+>>> ''')
     if fileFolder == "file":
         file = open(path)
         file.close()
     elif fileFolder == "folder":
         os.mkdir(path)
     else:
-        os.system("cls")
-        os.system("py ./adminTerminal.py -errorShow")
+        os.system(f"{cls_command}")
+        os.system("{py_command} ./adminTerminal.py -errorShow")
 
-    os.system("py ./adminTerminal.py")
+    os.system("{py_command} ./adminTerminal.py")
 except:
-    os.system("py ./adminTerminal.py")""")
+    os.system("{py_command} ./adminTerminal.py")""")
         crt.close()
-        os.system("cls")
+        os.system(f"{cls_command}")
         print("Cleaning Up. . .")
         loading_bar(20)
-        os.system("cls")
+        os.system(f"{cls_command}")
         if debug == True:
             print("Terminal Activated!")
         terminal()
@@ -185,7 +237,7 @@ except:
                 quit()
 except Exception as err:
     print(f"{text_decor.style.END}")
-    os.system("cls")
+    os.system(f"{cls_command}")
     lineNum = traceback.format_exc()
     if debug == True:
         msg.showerror("Admin Terminal", f"Admin Terminal has been terminated due to an exception\nError: {err}\n\nFull Error: {lineNum}")
@@ -193,5 +245,5 @@ except Exception as err:
         msg.showerror("Admin Terminal", "Admin Terminal has been terminated from an unknown crash")
 except KeyboardInterrupt:
     print(f"{text_decor.style.END}")
-    os.system("cls")
+    os.system(f"{cls_command}")
     msg.showerror("Admin Terminal", "Admin Terminal has been terminated from a keyboard interruption")
